a660aa160390e7e885fdee7a176fbd976158b8d7
sakshipaikra73 <sakshipaikra73@gmail.com>
2025-09-22 15:41:06 +0530

    Add files via upload

---
+++ SubmissionRepository.java
@@ -0,0 +1,9 @@
+package com.example.webhooksolver.repo;
+
+import com.example.webhooksolver.model.Submission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubmissionRepository extends JpaRepository<Submission, Long> {
+}
